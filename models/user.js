464406a55e8a22a30203e5914e1263bf96099214
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-07-10 20:01:23 +0300

    add models and db connection

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  member: { type: Boolean },
+});
+
+UserSchema.virtual("name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.last_name) {
+    fullname = `${this.first_name} ${this.family_name}`;
+  }
+  return fullname;
+});
+
+module.exports = mongoose.model("User", UserSchema);
